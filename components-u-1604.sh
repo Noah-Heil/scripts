1803330d043b29515afb1c8eb67481f3662b23b9
Noah-Heil <nceheil@gmail.com>
2017-05-22 14:02:11 -0600

    lets automate this gcloud install

---
+++ components-u-1604.sh
@@ -0,0 +1,19 @@
+export CLOUDSDK_CORE_DISABLE_PROMPTS=1
+gcloud components update -q
+gcloud components install datalab -q
+gcloud components install alpha -q
+gcloud components install beta -q
+gcloud components install bq -q
+gcloud components install cbt -q
+gcloud components install core -q
+gcloud components install gsutil -q
+gcloud components install app-engine-python -q
+gcloud components install kubectl -q
+gcloud components install app-engine-java -q
+gcloud components install docker-credential-gcr -q
+gcloud components install emulator-reverse-proxy -q
+gcloud components install pubsub-emulator -q
+gcloud components install gcd-emulator -q
+gcloud components install app-engine-go -q
+gcloud components install cloud-datastore-emulator -q
+gcloud components install gcloud -q
---
+++ gcloud-u-1604.sh
@@ -0,0 +1,5 @@
+wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-155.0.0-linux-x86.tar.gz
+tar -xvf google-cloud-sdk-155.0.0-linux-x86.tar.gz
+cd google-cloud-sdk
+./install.sh
+
